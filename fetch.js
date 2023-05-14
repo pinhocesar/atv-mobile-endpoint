e4559147d9b2f47bc67095606c5bd74364369c41
pinhocesar <caesaraugustus1806@gmail.com>
2023-05-14 05:01:57 -0300

    adiciona carregamento do json na página

--- fetch.js
+++ fetch.js
@@ -1,4 +1,21 @@
-//Funcões para chamar endpoints
-func = (met) => {
-    alert(`carregou as budega com ${met}`);
+async function carregaLista() {
+    const response = await fetch("http://127.0.0.1:1880/listar");
+    const jsonData = await response.json();
+    
+    console.log(jsonData);
+
+    let corpoTabela = document.getElementById('corpo_tabela');
+    let listaFormatada = [];
+
+
+    for (let item of jsonData) {
+        let registroFormatado = `<tr><th scope="row">${item.id}</th>
+        <td>${item.nome}</td>
+        <td>${item.cpf}</td>
+        <td>${item.dt_nasc}</td></tr>`;
+
+        listaFormatada.push(registroFormatado);
+    }
+
+    corpoTabela.innerHTML = listaFormatada.join('');
 }
