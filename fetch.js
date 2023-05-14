9c46fb4ffe9a0070a5c0a9e161cd0310f26c3f83
pinhocesar <caesaraugustus1806@gmail.com>
2023-05-14 07:59:51 -0300

    adiciona lógica de inserção na página

--- fetch.js
+++ fetch.js
@@ -1,12 +1,9 @@
 async function carregaLista() {
     const response = await fetch("http://127.0.0.1:1880/listar");
     const jsonData = await response.json();
-    
-    console.log(jsonData);
 
     let corpoTabela = document.getElementById('corpo_tabela');
     let listaFormatada = [];
-
 
     for (let item of jsonData) {
         let registroFormatado = `<tr><th scope="row">${item.id}</th>
@@ -18,4 +15,24 @@ async function carregaLista() {
     }
 
     corpoTabela.innerHTML = listaFormatada.join('');
+}
+
+async function novoRegistro() {
+    let nome = document.getElementById('input_nome').value;
+    let cpf = document.getElementById('input_cpf').value;
+    let dt_nasc = document.getElementById('input_dt_nasc').value;
+
+    let corpo_req = {
+        "nome": nome,
+        "cpf": cpf,
+        "dt_nasc": dt_nasc
+    }
+
+    const response = await fetch("http://127.0.0.1:1880/inserir", {
+        method: 'POST',
+        body: JSON.stringify(corpo_req),
+        headers: {"Content-Type": "application/json; charset=UTF-8"}
+    });
+
+    const responseJson = await response.json();
 }
